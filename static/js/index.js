3b2c33fdc927e7bafe9c87eeb0a295b20e5cdb5e
Rahmatullah. R <mamatmks45@gmail.com>
2023-08-29 16:34:29 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -58,7 +58,6 @@ function openCvReady() {
                     canvasCtx.textAlign = "center";
                     
                     // Hide the "No face found" message
-                    roiCtx.style.display = 'block';
                     noFaceFoundElement.style.display = 'none';
                     if (tfliteModel && isModelLoaded) {
                         // Perform the prediction
@@ -88,7 +87,6 @@ function openCvReady() {
                 } else {
                     // No face detected
                     roiCtx.clearRect(0, 0, canvasRoi.width, canvasRoi.height);
-                    roiCtx.style.display = 'none';
                     noFaceFoundElement.style.display = 'block';
                 }
                 canvasCtx.restore();
